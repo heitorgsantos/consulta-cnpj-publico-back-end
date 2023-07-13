cd4da0a0838ba73433a6da746e36ba5e861f0e45
Heitor Santos <heitorg33@gmail.com>
2023-07-12 21:05:08 -0300

    Validando {   origin: 'http://localhost:3000', } no cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,11 @@ const { router } = require("./routes/router");
 // router
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use(
@@ -17,6 +21,6 @@ app.use(
   })
 );
 
-app.use('/', router);
+app.use("/", router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
