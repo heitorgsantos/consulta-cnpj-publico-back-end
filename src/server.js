2d77b3a71b22c472ff0e804361bc15ccc8c57a96
Heitor Santos <heitorg33@gmail.com>
2023-07-22 12:37:48 -0300

    Correção da porta

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 const { router } = require("./routes/router");
 // const { router } = require("./routes/router");
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT ?? 8080;
 const app = express();
 app.use(
   cors({
